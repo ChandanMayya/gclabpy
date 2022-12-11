47c2c8e9bf639f1e04ab367e7e03c9e3c3f67508
Chandan Mayya <59369165+ChandanMayya@users.noreply.github.com>
2022-12-11 18:20:21 +0530

    Update srt_test.py

--- srt_test.py
+++ srt_test.py
@@ -1,17 +1,23 @@
-
 lst=[]
 n=int(input("Enter max: "))
 for i in range(0,n):
     ele = int(input("enter element:"))
     lst.append(ele)
 lst2=sorted(lst)
+print(lst2)
 e=int(input("Enter a element to insert: "))
 for i in range(len(lst2)):
-    if ele<lst2[i]:
-        lst2.insert(i+1,ele)
+    if lst2[i]>e:
+        lst2.insert(i,e)
+        break
+    elif lst2[len(lst2)-1] < e:
+        lst2.insert(len(lst2),e)
+        break
 print(lst2)
 
 
     
+
+
 
 
