b991498e4ec5611a4473ae04039eaf3e7f6d03f0
Chandan Mayya <59369165+ChandanMayya@users.noreply.github.com>
2023-01-05 12:00:38 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -6,3 +6,6 @@ def mul(a,b):
 
 def div(a,b):
     print (a/b)
+
+    
+https://www.researchgate.net/publication/354737842_Moisture_content_and_water_activity_of_arecanut_samples_A_need_to_revisit_storage_guidelines
